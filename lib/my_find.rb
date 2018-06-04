8710f0ba51d95d6d20cb540d4c31c440adf90b4e
Jed Peek <jedpeek@yahoo.com>
2018-06-04 23:01:20 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_find(collection)
-
+i = 0
+while i < collection.size
+  yield(collection[i])
+  i += 1
 end
+end
+
+my_find(){|x|
+  if x % 3 == 0 && x % 5 == 0
+    puts "FizzBuzz"
+  end
+}
